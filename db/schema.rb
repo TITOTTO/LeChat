ae0a1ca4a4026efaf72f68b397392ac3edf5370b
widmaker666 <alexandre.hontcharouk69@gmail.com>
2022-11-28 11:45:24 +0100

    page panier créer faire les liaisons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_083632) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_104106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_083632) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -49,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_083632) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "carts", "users"
 end
