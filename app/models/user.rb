432b5b6fc7f19c6fa5109becdde87d574da03c73
Hugodujour <hugogreg@live.fr>
2022-11-28 17:08:00 +0100

    Login/signout + CSS float

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :articles
   has_many :comments, as: :commentable
+  
+  # Syntaxe active storage
+  has_one_attached :avatar
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/show'
   resources :comments
   resources :articles
   devise_for :users
