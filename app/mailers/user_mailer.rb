d9f7ae82ef3333c6b812589440a83c349c0247ea
0101spbl <spbl@outlook.fr>
2022-11-29 15:18:45 +0100

    mailer update2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,13 @@
 class UserMailer < ApplicationMailer
-    default from: 'kuaker26@gmail.com'
-
-    def participate_email(attendance)
-       @user = User.find(attendance.user_id)
-       @event = Event.find(attendance.event_id)
-       @url = "https://eventbritetest2.herokuapp.com/"
-       mail(to: @user.email, subject: 'Vous êtes inscrit')
-    end
+   default from: 'noreply@cat-e-store.com'
  
-  
+   def welcome_email(user)
+     @user = user
+     mail(to: @user.email, subject: 'Chat alors! Bienvenue')
+   end
  
-     def welcome_email(user)
-      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user 
-         @user = user
-         
-      #on définit une variable @url qu'on utilisera dans la view d’e-mail 
-         @url  = 'http://monsite.fr/login'
-
-      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet. 
-         mail(to: @user.email, subject: 'Bienvenue chez nous !')
- 
-     end 
-end
+   def order_confirm(user)
+     @user = user
+     mail(to: @user.email, subject: "Confirmation d'aChAT")
+   end
+ end
