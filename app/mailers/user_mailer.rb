4b3f487edb7eb7d2e1be59c7a353f955b7180a87
TITOTTO <114482795+TITOTTO@users.noreply.github.com>
2022-11-30 10:01:12 +0100

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,8 @@
 class UserMailer < ApplicationMailer
-<<<<<<< HEAD
-    default from: 'kuaker26@gmail.com'
 
     def participate_email(attendance)
     end
-=======
    default from: 'noreply@cat-e-store.com'
->>>>>>> c49db2774ccde6824bc5838a3d1418393ff98f78
  
    def welcome_email(user)
      @user = user
